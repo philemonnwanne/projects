d206517a7c441c5c5982eb403348d132c4ee49d5
Philemon Nwanne <108567784+philemonnwanne@users.noreply.github.com>
2022-10-13 06:15:15 +0100

    Update script.sh

--- exercises/exe-07/script.sh
+++ exercises/exe-07/script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# Usage: A sample shell script to save system memory (RAM) usage at every hour, and forward cummulative hourly memory usage to sys_admin at 00:00 hours
+# Usage: A sample shell script to save system memory (RAM) usage at every hour, and forward cumulative hourly memory usage to sys_admin at 00:00 hours
 
 # Author: Mr Root
 
@@ -9,83 +9,82 @@ sys_admin="Mr Root"
 admin_mail="root@gmail.com"
 
 # Source and destination files
-S='/home/vagrant/script.sh'
-D='/home/vagrant/logs/logfile.log'
-DIR='/home/vagrant/logs'
+SCRIPT_SRC='/home/vagrant/script.sh'
+LOG_FILE='/home/vagrant/logs/log_file.log'
+LOG_DIR='/home/vagrant/logs'
 
-# Create a logs directory in "/vagrant" but if exits skip
-if [ "$PWD" == "$DIR" ]; 
+# Create a logs directory in "/home/vagrant" but if exits skip
+if [ "$PWD" == "$LOG_DIR" ]; 
 then  
     echo "Directory Already Exits!"
 else
-    mkdir -p $DIR
+    mkdir $LOG_DIR
 fi
 
-# Switch to the "/vagrant/logs" directory
+# Switch to the "/home/vagrant/logs" directory
 cd /home/vagrant/logs
 
 # To create an empty log file
-touch logfile.log
+touch log_file.log
 
 # Date & Time header
-echo "CURRENT DATE/TIME" >> ${D}
+echo "CURRENT DATE/TIME" >> ${LOG_FILE}
 
 # Just a demarcation
-echo "------------------------------------" >> ${D}
+echo "------------------------------------" >> ${LOG_FILE}
 
-# Display the curent date and time in UTC format
-date >> ${D}
+# Display the current date and time in UTC format
+date >> ${LOG_FILE}
 
 # Insert an empty line
-echo "" >> ${D}
+echo "" >> ${LOG_FILE}
 
 # Memory usage header
-echo "CURRENT MEMORY USAGE" >> ${D}
+echo "CURRENT MEMORY USAGE" >> ${LOG_FILE}
 
 # Just another demarcation
-echo "-----------------------------------------------------------------------------------------------" >> ${D}
+echo "-----------------------------------------------------------------------------------------------" >> ${LOG_FILE}
 
 # Display the memory usage statistics
-free >> ${D}
+free >> ${LOG_FILE}
 
 # Insert an empty line
-echo "" >> ${D}
+echo "" >> ${LOG_FILE}
 
 # Log maintainer header
-echo "LOG MAINTAINER" >> ${D}
+echo "LOG MAINTAINER" >> ${LOG_FILE}
 
 # Just another demarcation
-echo "-------------------------------------------------------" >> ${D}
+echo "-------------------------------------------------------" >> ${LOG_FILE}
 
 # This command displays the systems administrator
-echo "This log file is being maintained by $sys_admin" >> ${D}
+echo "This log file is being maintained by $sys_admin" >> ${LOG_FILE}
 
 # Insert double empty lines
-echo "" >> ${D}; echo "" >> ${D}
+echo "" >> ${LOG_FILE}; echo "" >> ${LOG_FILE}
 
-echo "                              🐦 NEW LOG ENTRY 🐦             " >> ${D}
+echo "                              🐦 NEW LOG ENTRY 🐦             " >> ${LOG_FILE}
 
-echo "" >> ${D}; echo "" >> ${D}
+echo "" >> ${LOG_FILE}; echo "" >> ${LOG_FILE}
 
 # This clears my terminal before running the command thereby keeping it less messy
 # clear -- For test purposes only
 
 # Display contents of the log file 
-# cat ${D} -- For test purposes only
+# cat ${LOG_FILE} -- For test purposes only
 
 # Send a copy of log file to the current users home directory
 # currentUsersHome=$HOME  -- For test purposes only
-# cp ${D} $HOME  -- For test purposes only
+# cp ${LOG_FILE} $HOME  -- For test purposes only
 
 # Send email to sys_admin at 12am
 currentTime=$(date +%H:%M)
 
 if [ ${currentTime} = 00:00 ];
-    then mail ${admin_mail} < ${D} &&
-    # Sleep for 10m minutes and delete the previous log file
+    then mail ${admin_mail} < ${LOG_FILE} &&
+    # Sleep for 10 minutes and delete the previous log file
     sleep 10m &&
-    rm -f ${D}
+    rm -f ${LOG_FILE}
 else
     :
 fi
-
